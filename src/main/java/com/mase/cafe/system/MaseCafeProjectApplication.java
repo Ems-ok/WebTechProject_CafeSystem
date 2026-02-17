59753731637ac4c02904c48a0ba3ca78a7ac286f
A00325365 <a00325365@student.tus.ie>
2026-02-17 15:22:34 +0000

    Initial commit - Cafe Management System

---
+++ src/main/java/com/mase/cafe/system/MaseCafeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mase.cafe.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaseCafeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaseCafeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mase/cafe/system/MaseCafeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mase.cafe.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaseCafeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
