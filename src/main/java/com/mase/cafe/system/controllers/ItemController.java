109c545538b78f88fbb94c725766e5051c382d4e
Emma O'Keeffe <a00325365@student.tus.ie>
2026-03-20 14:45:04 +0000

    pipeline test

--- src/main/java/com/mase/cafe/system/controllers/ItemController.java
+++ src/main/java/com/mase/cafe/system/controllers/ItemController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//Pipline test
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/manager/api/items")
